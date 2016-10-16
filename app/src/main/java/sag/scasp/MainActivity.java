d48d8f092266186eeb470a3e8ad21330cc52a3cb
RaisinghRajput <rai.sodha@gmail.com>
2016-10-16 11:39:18 +0500

    Nothing changed aga

--- app/src/main/java/sag/scasp/MainActivity.java
+++ app/src/main/java/sag/scasp/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+ //nothinsdfnas
+
         setContentView(R.layout.activity_main);
     }
 }
